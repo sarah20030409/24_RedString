f7605bffa8176cd83d02ba41185ef2da08da11a8
Your Name <a0908647929@gmail.com>
2024-09-19 17:38:43 +0800

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="bg-black h-screen">
       <header>
         <div>
-          <p className="text-3xl font-bold text-white">hii你好</p>
+          <p className="text-3xl font-bold text-white">你好</p>
         </div>
       </header>
     </div>
