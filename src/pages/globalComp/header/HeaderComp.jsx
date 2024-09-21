74cebf20866e2c65ed85ca7525738e7ccc91a94c
Your Name <a0908647929@gmail.com>
2024-09-21 23:40:27 +0800

    Anim setup & NavBar

--- src/pages/globalComp/header/HeaderComp.jsx
+++ src/pages/globalComp/header/HeaderComp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../../../assets/imgs/header/Logo.svg";
+import NavLine from "../../../assets/imgs/header/NavLine.svg";
 import { Link } from "react-router-dom";
 
 export default function Header(){
@@ -9,17 +10,20 @@ export default function Header(){
         {to:"/sponsor",text:"贊助廠商"},
     ]
     return(
-        <div className="h-20 bg-white px-40 py-1 ">
+        <div className="h-20 bg-white lg:px-40 py-1 md:px-10 sm:px-10">
             <div className="flex justify-between items-center">
                 <div className="flex ">
-                    <Link to="/"><img src={Logo} alt="赤弦獎" /></Link>
+                    <Link to="/"><img className="scale-90 sm:scale-75" src={Logo} alt="赤弦獎" /></Link>
                 </div>
-                <nav>
+                <nav className="hidden md:block">
                     <ul className="flex justify-between items-center">
                         {navItems.map((item,index) => (
                             <li className="mx-5" key={index}><Link className="NavText" to={item.to}>{item.text}</Link></li>
                         ))}
                     </ul>
+                </nav>
+                <nav className="md:hidden">
+                    <img src={NavLine} alt="NavLine" />
                 </nav>
             </div>
         </div>
