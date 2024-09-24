c955288be5aea2ffd500d86c9d222f30f1d5e3ea
Your Name <a0908647929@gmail.com>
2024-09-24 15:29:28 +0800

    Bug fix

--- src/pages/loadingPage/loadingPage.jsx
+++ src/pages/loadingPage/loadingPage.jsx
@@ -4,7 +4,7 @@ import OpenTitle from "../../assets/imgs/homePage/OpenTitle.svg"
 export default function LoadingPage(){
     return(
         <div className="bg-black h-screen flex justify-center items-center ">
-            <div className="animate-fadeInOut max-lg:-mt-[140px] max-md:-mt-[100px]">
+            <div className="animate-fadeInOut max-lg:-mt-[150px] max-md:-mt-[100px]">
             <img src={OpenTitle} alt="醉聲" className="scale-75 max-md:scale-[70%]"/>
             <h1 className="SmallTitle tracking-[5px] font-light text-RSredDeep text-center" data-aos="fade-up">24rd Red String</h1>
         </div>
