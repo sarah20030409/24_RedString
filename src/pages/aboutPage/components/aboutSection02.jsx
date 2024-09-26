7b9a65113d54575affb9bc331ddd124257c439f5
Your Name <a0908647929@gmail.com>
2024-09-26 18:49:27 +0800

    About page test

--- src/pages/aboutPage/components/aboutSection02.jsx
+++ src/pages/aboutPage/components/aboutSection02.jsx
@@ -19,11 +19,11 @@ export default function AboutSection02() {
     }
 
     return(
-        <div className="pt-56 pb-48 max-lg:pt-38 ">
+        <div className="pt-56 pb-48 max-lg:pt-38 max-lg:pb-40 max-md:pt-32 max-md:pb-24">
             <div data-aos="fade-up" className="flex justify-around px-[15%]  max-lg:px-[10%] ">
-                <div className="w-52 hover:cursor-pointer hover:scale-105 max-lg:w-[24%]" onClick={() => {handleOpen(0)}} ><img src={goldBall01} alt="red string"/></div>
-                <div className="w-52 hover:cursor-pointer hover:scale-105 max-lg:w-[24%]" onClick={() => {handleOpen(1)}}><img src={goldBall02} alt="red string"/></div>
-                <div className="w-52 hover:cursor-pointer hover:scale-105 max-lg:w-[24%]" onClick={() => {handleOpen(2)}}><img src={goldBall03} alt="red string"/></div>
+                <div className="w-52 hover:cursor-pointer hover:scale-105 max-lg:w-[24%] " onClick={() => {handleOpen(0)}} ><img src={goldBall01} alt="red string"/></div>
+                <div className="w-52 hover:cursor-pointer hover:scale-105 max-lg:w-[24%] " onClick={() => {handleOpen(1)}}><img src={goldBall02} alt="red string"/></div>
+                <div className="w-52 hover:cursor-pointer hover:scale-105 max-lg:w-[24%] " onClick={() => {handleOpen(2)}}><img src={goldBall03} alt="red string"/></div>
             </div>
             <div className="pt-40 max-lg:pt-24" ref={targetRef}>
                 <About02Info number={selectedNumber} />
--- src/pages/aboutPage/components/aboutSection03.jsx
+++ src/pages/aboutPage/components/aboutSection03.jsx
@@ -6,13 +6,13 @@ import About03Info from "./about03Info";
 
 export default function AboutSection03() {
    return(
-    <div className="bg-contain bg-no-repeat bg-top mb-48" style={{backgroundImage: `url(${bgCloud01})`}}>
+    <div className="bg-contain bg-no-repeat bg-top mb-48 max-md:mb-40" style={{backgroundImage: `url(${bgCloud01})`}}>
         <div className="bg-contain bg-no-repeat bg-bottom" style={{backgroundImage: `url(${bgCloud02})`}}>
         <div className="pb-40 max-lg:pb-28">
-            <p className="SectionTitle tracking-[5px] text-center">籌備團隊</p>
+            <p className="SectionTitle tracking-[5px] text-center max-lg:pb-2">籌備團隊</p>
             <p className="TsaiYin text-center pt-1">&#123;采音吉他社&#125;</p>
         </div>
-        <div className="bg-contain bg-no-repeat bg-center" style={{backgroundImage: `url(${BigRed})`}}>
+        <div className="bg-contain bg-no-repeat bg-center max-md:bg-cover" style={{backgroundImage: `url(${BigRed})`}}>
             <div data-aos="fade-up" className="MemberDisplaySetup pt-0">
             <About03Info memberNum={0}/>
             <About03Info memberNum={1}/>
