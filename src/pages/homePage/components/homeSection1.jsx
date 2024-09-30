a9baeda55f060a9f1d0ffdf4d57e985d28e8614b
Your Name <a0908647929@gmail.com>
2024-09-30 12:55:22 +0800

    bug fix

--- src/pages/homePage/components/homeSection1.jsx
+++ src/pages/homePage/components/homeSection1.jsx
@@ -23,9 +23,7 @@ export default function HomeSection1() {
             className="bg-contain bg-no-repeat bg-center flex justify-center w-[300px] "
             style={{ backgroundImage: `url(${titleFrame})` }}
           >
-            <h1 className="SectionTitle text-nowrap p-[1.5px] m-[2px]">
-              比賽資訊
-            </h1>
+            <h1 className="SectionTitle text-nowrap  m-[4px]">比賽資訊</h1>
           </div>
         </div>
 
